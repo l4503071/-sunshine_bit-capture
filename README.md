457a2f1baab13770cf738b4a62d3446eaeb943f4
sunshine <sunliang3@xiaomi.com>
2019-05-28 16:02:51 +0800

    add:puppeteer

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # sunshine_bit@capture
 
+[npm](https://img.shields.io/node/v/@sunshine_bit/capture.svg)
+
 > 截取指定 url 的屏幕信息,可以指定屏幕的宽和高.
 
 ## 安装
 
+```js
+npm i @sunshine_bit/capture
+````
+
 ## 参数
 
-## 实例
+- `options` [object]
+  - url <string> default: `'https://github.com/l4503071'`,
+  - width <number> default: `375`,
+  - height <number> default: `667`,
+  - deviceScaleFactor <number> default: `2`,
+  - fileName <string> default: `'capture.png'`,
+
+> TODO: 其他参数覆盖,参考 (`./devices.js`)
+
+## 事例
 
--- index.js
+++ index.js
@@ -1,5 +1,40 @@
+const puppeteer = require('puppeteer');
+const devices = require('puppeteer/DeviceDescriptors');
+const ip6 = devices['iPhone 6'];
+
 module.exports = {
-  test() {
-    console.log('test');
+  async capture(options = {}) {
+    const {
+      url = 'https://github.com/l4503071',
+      width = 375,
+      height = 667,
+      deviceScaleFactor = 2,
+      fileName = 'capture.png',
+    } = options;
+    const browser = await puppeteer.launch({
+      headless: true,  // 无 UI 模式
+      ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    const params = {
+      width,
+      height,
+      deviceScaleFactor,
+    };
+    for(key in params) {
+      const value = params[key];
+      if(!!key && typeof value === 'number' && !!ip6.viewport[key]) {
+        ip6.viewport[key] = value;
+      }
+    }
+    await page.emulate(ip6);
+    await page.goto(url, {
+      timeout: 30000,
+      waitUntil: 'networkidle2',
+    });
+    await page.screenshot({
+      path: fileName,
+    });
+    await browser.close();
   }
 }
